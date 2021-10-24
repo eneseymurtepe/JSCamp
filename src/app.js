ad28e46dca8706e61c1c4c5e40547fd2ddc387f6
Enes EYMURTEPE <eymurtepeenes@gmail.com>
2021-10-24 17:28:02 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+
+// JS type safe değildir.
+
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+// euroDun = 11 => const ile oluşturulan bir değişkenin değeri değiştirilemez.
+
+console.log(euroDun)
+
+// Array 
+// camelCasing
+// PascalCasing
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
+
+for(let i = 0; i < konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+}
+
+
+console.log(konutKredileri)
